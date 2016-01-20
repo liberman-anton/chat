a6d0f31dcffe5ad29143b3b1f78351e352e163d4
liberman-anton <antonliberman@gmail.com>
2016-01-20 13:46:29 +0500

    write br

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         ServerSocket ss = new ServerSocket(8083);
+
+        Set<Socket> clients = new HashSet<>();
 
         while (true) {
             Socket accept = ss.accept();
@@ -13,8 +17,17 @@ public class Main {
                 @Override
                 public void run() {
                     try {
+                        InputStream inputStream = accept.getInputStream();
+                        OutputStream outputStream = accept.getOutputStream();
+                        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
                         while (true) {
-                            accept.getOutputStream().write("Hello".getBytes());
+                            String line = br.readLine();
+                            if ("exit".equals(line))
+                                break;
+                            for (Socket s : clients) {
+                                s.getOutputStream().write(line.getBytes());
+                            }
+
                             try {
                                 Thread.sleep(1000);
                             } catch (InterruptedException e) {
@@ -26,6 +39,9 @@ public class Main {
                     }
                 }
             }).start();
+
+            clients.add(accept);
+
         }
     }
 }
